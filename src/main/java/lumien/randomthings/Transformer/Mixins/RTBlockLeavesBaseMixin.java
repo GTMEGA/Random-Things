5b98b897372b239e5edae4063798804146c9f295
FalsePattern <30945458+FalsePattern@users.noreply.github.com>
2022-01-14 15:16:17 +0100

    Fix fast leaf decay

--- src/main/java/lumien/randomthings/Transformer/Mixins/RTBlockLeavesBaseMixin.java
+++ src/main/java/lumien/randomthings/Transformer/Mixins/RTBlockLeavesBaseMixin.java
@@ -1,6 +1,5 @@
 package lumien.randomthings.Transformer.Mixins;
 
-import com.sun.xml.internal.bind.v2.TODO;
 import lumien.randomthings.Handler.CoreHandler;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockLeavesBase;
@@ -10,8 +9,7 @@ import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(BlockLeavesBase.class)
 public abstract class RTBlockLeavesBaseMixin {
-    //TODO: doesnt work
-    @Intrinsic()
+    @Intrinsic(displace = true)
     public void onNeighborBlockChange(World p_149695_1_, int p_149695_2_, int p_149695_3_, int p_149695_4_, Block p_149695_5_) {
         CoreHandler.handleLeaveDecay(p_149695_1_, p_149695_2_, p_149695_3_, p_149695_4_, (Block)(Object)this);
     }
